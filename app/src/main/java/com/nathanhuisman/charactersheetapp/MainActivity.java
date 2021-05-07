fab3a7c3861c84aec3eac75b29972b2ba286451e
Nathan Huisman <nathanhuisman471@gmail.com>
2021-05-07 13:38:00 -0500

    added functionality to the home page and did some basic formatting. Also deleted a rouge page in the program and did some code simplification.

--- app/src/main/java/com/nathanhuisman/charactersheetapp/MainActivity.java
+++ app/src/main/java/com/nathanhuisman/charactersheetapp/MainActivity.java
@@ -2,25 +2,53 @@ package com.nathanhuisman.charactersheetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     public String[] characterClasses = {"Barbarian", "Champion", "Fighter", "Ranger", "Rouge"};
 
+    private EditText characterName;
+    private Spinner chosenClass;
+
+    ImageButton swordImageButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spinner = findViewById(R.id.selectInitialClass);
+        swordImageButton = findViewById(R.id.mainNextButton);
+
+        chosenClass = findViewById(R.id.selectInitialClass);
         ArrayAdapter<String>adapter = new ArrayAdapter<>(MainActivity.this,
                 android.R.layout.simple_spinner_item,characterClasses);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        chosenClass.setAdapter(adapter);
+
+        characterName = findViewById(R.id.characterNameEntry);
+    }
+
+
+
+    //when the sword next button is clicked, grab the chosen class and name and give it to the buildCharacter class
+    public void onNextButtonClick(View view){
+
+        int selectedClass = chosenClass.getSelectedItemPosition();
+
+        String playerName = characterName.getText().toString();
+        String playerClass = chosenClass.getItemAtPosition(selectedClass).toString();
+
+        Intent intent = new Intent(this,buildCharacter.class);
+        intent.putExtra("CHARACTER_NAME", new String[] {playerName, playerClass});
+        startActivity(intent);
     }
 
 
---
+++ app/src/main/java/com/nathanhuisman/charactersheetapp/buildCharacter.java
@@ -0,0 +1,14 @@
+package com.nathanhuisman.charactersheetapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class buildCharacter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_build_character);
+    }
+}
